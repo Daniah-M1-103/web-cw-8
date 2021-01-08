6caddaa5efd2fb4ec59d69f85f8e7fa457a06edb
Dania-M1-103 <191406@student.kcst.edu.kw>
2021-01-08 16:48:11 +0300

    done

--- myscript.js
+++ myscript.js
@@ -1,12 +1,60 @@
 let board = [
-    ['00', '01', '02'],
-    ['10', '11', '12'],
-    ['20', '21', '22']
+    ['', '', ''],
+    ['', '', ''],
+    ['', '', '']
 ]
 
 
+let turn= "o";
 
-function press(cellNO) {
+function press(x, y) {
+if(turn == "o") {
+    turn= "x";
+} else {
+    turn= "o";
+}
+
+board[x][y]= turn;
+let cell=document.getElementById(`${x}${y}`);
+cell.innerText= turn;
+
+if (board[0][0]= turn && board[0][1]==turn && board[0][2]==turn){
+    document.getElementById("status").innerText=`Winner is ${turn}`;
+}
+
+if (board[1][0]= turn && board[1][1]==turn && board[1][2]==turn){
+    document.getElementById("status").innerText=`Winner is ${turn}`;
+}
+
+if (board[1][0]= turn && board[1][1]==turn && board[1][2]==turn){
+    document.getElementById("status").innerText=`Winner is ${turn}`;
+}
+
+if (board[2][0]= turn && board[2][1]==turn && board[2][2]==turn){
+    document.getElementById("status").innerText=`Winner is ${turn}`;
+}
+
+if (board[0][0]= turn && board[1][0]==turn && board[2][0]==turn){
+    document.getElementById("status").innerText=`Winner is ${turn}`;
+}
+
+if (board[0][1]= turn && board[1][1]==turn && board[2][1]==turn){
+    document.getElementById("status").innerText=`Winner is ${turn}`;
+}
+
+if (board[0][2]= turn && board[1][2]==turn && board[2][2]==turn){
+    document.getElementById("status").innerText=`Winner is ${turn}`;
+}
+
+if (board[0][0]= turn && board[1][1]==turn && board[2][2]==turn){
+    document.getElementById("status").innerText=`Winner is ${turn}`;
+}
+
+if (board[0][2]= turn && board[1][1]==turn && board[2][0]==turn){
+    document.getElementById("status").innerText=`Winner is ${turn}`;
+}
+
+
 
     // Do stuff
     console.log(cellNO)
